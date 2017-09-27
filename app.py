715d6c18861db43e13c5714b8f46bc35ff35eaac
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2017-09-27 09:34:11 +0900

    remove start.sh

--- app.py
+++ app.py
@@ -7,25 +7,28 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # maximum upload size
 ALLOWED_EXTENSIONS = set(['png'])
 
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/convert', methods=['POST'])
 def convert():
-	if 'file' not in request.files:
-		abort(400)
-	file = request.files['file']
-	print("filename: " + file.filename)
-	if file.filename == '':
-		abort(400)
-	if file and allowed_file(file.filename):
-		contents = file.read()
-		filename = hashlib.sha256(contents).hexdigest() + '.png'
-		path = os.path.join('/tmp', filename)
-		with open(path, 'w') as f:
-			f.write(contents)
-		print(path)
-		return send_file(path, mimetype='image/png');
+    if 'file' not in request.files:
+        abort(400)
+    file = request.files['file']
+    print('filename: ' + file.filename)
+    if file.filename == '':
+        abort(400)
+    if file and allowed_file(file.filename):
+        contents = file.read()
+        filename = hashlib.sha256(contents).hexdigest() + '.png'
+        path = os.path.join('/tmp', filename)
+        with open(path, 'w') as f:
+            f.write(contents)
+        print(path)
+        return send_file(path, mimetype='image/png');
 
 @app.route('/')
 def root():
-	return app.send_static_file('index.html')
+    return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', debug=True)
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env /bin/bash
-set -e
-FLASK_DEBUG=1 FLASK_APP=app.py flask run --host=0.0.0.0
-
