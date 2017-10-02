2af5d32dccfda9dc15b21739f31fbff1546fffa4
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2017-10-02 09:35:10 +0900

    integrate fast-style-transfer

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import hashlib
+import sys
 import os
+
 from flask import Flask, request, send_file
 app = Flask(__name__, static_url_path='')
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # maximum upload size
+
+sys.path.append('fast-style-transfer')
+sys.path.append('fast-style-transfer/src') # A terrible hack
+from evaluate import ffwd_to_img
 
 ALLOWED_EXTENSIONS = set(['png'])
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+CHECKPOINT_FILE = 'fast-style-transfer/models/udnie.ckpt'
 
 @app.route('/convert', methods=['POST'])
 def convert():
@@ -19,12 +27,18 @@ def convert():
         abort(400)
     if file and allowed_file(file.filename):
         contents = file.read()
-        filename = hashlib.sha256(contents).hexdigest() + '.png'
-        path = os.path.join('/tmp', filename)
-        with open(path, 'bw') as f:
-            f.write(contents)
-        print(path)
-        return send_file(path, mimetype='image/png');
+        digest = hashlib.sha256(contents).hexdigest()
+        in_filename = digest + '.png'
+        in_path = os.path.join('/tmp', in_filename)
+        out_filename = digest + '_conv.png'
+        out_path = os.path.join('/tmp', out_filename)
+
+        if not os.path.exists(out_path):
+            with open(in_path, 'bw') as f:
+                f.write(contents)
+            ffwd_to_img(in_path, out_path, CHECKPOINT_FILE)
+
+        return send_file(out_path, mimetype='image/png');
 
 @app.route('/')
 def root():
--- shell.nix
+++ shell.nix
@@ -2,7 +2,9 @@ with import <nixpkgs> {};
 
 (pkgs.python35.buildEnv.override {
   extraLibs = with pkgs.python35Packages; [
+    scipy
     flask
     tensorflow
+    pillow
   ];
 }).env
